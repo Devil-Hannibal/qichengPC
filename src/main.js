e0d94a7e3fdeae0ab7297eababd561623b2349c7
Devil-Hannibal <bfzy4444@163.com>
2019-05-14 09:58:20 +0800

    项目配置初始化

--- src/main.js
+++ src/main.js
@@ -2,11 +2,23 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui';		//引入饿了么-ui库
+import 'element-ui/lib/theme-chalk/index.css';   //引入饿了么 UI库样式
+
+import axios from './plugin/http.js'	//数据请求封装Axios
+
+import './assets/base.css';				 //基本样式
+import './../static/iconfont/iconfont.css'		//引入字体样式
+
+Vue.prototype.$axios=axios
+
+Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  axios,
   render: h => h(App)
 })
